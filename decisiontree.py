1db4e1e24c5f4e3bf517aecc8971537ff179ef62
Brayan <brayan.f.a07@hotmail.com>
2018-04-27 00:00:40 -0600

    Cambios en el archivo decisiontree.py 2

--- decisiontree.py
+++ decisiontree.py
@@ -1,22 +1,39 @@
-import from.tec.ic.ia.p1.g09 import generar_muestra_pais, generar_muestra_provinca
-
+from tec.ic.ia.pc1.g09 import generar_muestra_pais, generar_muestra_provincia
 
 
 ##Creación de la clase árbol de decisión
 class DecisionTree:
 	
 
-	"""docstring for DecisionTree"""
+	"""Initilization of the tree"""
 	def __init__(self):
 		self.nodes = []
 
 
-## Creación de la clase nodo para el árbol de decisiones
-class Node:
-	"""docstring for Node"""
-	def __init__(self, attribute, value):
-		self.attribute = attribute
-		self.value = value
+	## Creation of the class node for the decision tree
+	class Node:
 		
+		"""Initilization of the class"""
+		def __init__(self, attribute):
+			self.attribute = attribute
+			self.value = ""
 
-		
+	##Calc the entropy of the attributes
+	def calc_entropy():
+		list_attributes = []
+
+		"""Insert kind of attributes inside the list_attributes"""
+		for i in range(len(data[0])):
+			list_attributes.append("attribute"+str(i))
+
+
+		"""Printing the list of the attributes"""
+		for i in range(len(list_attributes)):
+			print(list_attributes[i])
+
+		return list_attributes
+
+print("Iniciando el programa...")
+data = generar_muestra_pais(5)
+x = DecisionTree()
+x.calc_entropy()
