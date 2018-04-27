209b75fc62b12b0474525ecc96bd58073d349bf2
Brayan <brayan.f.a07@hotmail.com>
2018-04-27 11:36:07 -0600

    Cambios en el método de cálculo de probabilidad

--- decisiontree.py
+++ decisiontree.py
@@ -7,54 +7,89 @@ list_political_parties=["ACCESIBILIDAD SIN EXCLUSION", "ACCION CIUDADANA", "ALIA
 
 
 ##Creación de la clase árbol de decisión
-class DecisionTree:
+"""class DecisionTree:
 	
 
-	"""Initilization of the tree"""
+	#Initilization of the tree
 	def __init__(self):
 		self.nodes = []
 
 	## Creation of the class node for the decision tree
 	class Node:
 		
-		"""Initilization of the class"""
+		#Initilization of the class
 		def __init__(self, attribute):
 			self.attribute = attribute
 			self.value = ""
+	"""
 
-	##Calc the entropy of the attributes
-	def calc_entropy():
-		list_attributes = []
+##Calc the entropy of the attributes
+def calc_entropy():
+	
+	attributes_list = calc_class_data(0, data)
+	probalistic_class_data = []
+
+	for i in range(len(attributes_list)):
+		probalistic_class_data.append(probability_in_list(attributes_list[i], attributes_list))
 
 
-		return list_attributes
 
-	##calc the class of an object 
-	def calc_class_data(position, dataList):
-		"""Insert kind of attributes inside the list_attributes"""
-		classDataList = []
-		for i in range(len(dataList)):
-			found = False
-			for j in range(len(classDataList)):
-				if (classDataList[j] == dataList[i][position])
-					found = True
+	return list_attributes
 
-			if found == False:
-				classDataList.append(dataList[i][position])
+#Calculate the probablistic to be found in a list
+def probability_in_list(element, list):
 
-		return classDataList
+	data_count = len(list)
+	probabability = found_count_in_list(element, list) / data_count
 
-	def printList(list):
+	return probabability
 
-		"""Printing the list of the attributes"""
-		for i in range(len(list)):
-			print(list[i])
+#Calculate the count of the elements in a list
+def found_count_in_list(element, list):
 
-		return 0
+	count = 0
+	for i in range(len(list)):
+		if (element == list[i]):
+			count = count + 1
+	return count
+
+
+#Calc the class of an object
+def calc_class_data(position, dataList):
+	
+
+	#Insert kind of attributes inside the list_attributes
+	classDataList = []
+	class_data_found_coun = []
+
+	for i in range(len(dataList)):
+		found = False
+		for j in range(len(classDataList)):
+			if (classDataList[j] == dataList[i][position]):
+				found = True
+
+		if found == False:
+			classDataList.append(dataList[i][position])
+
+	printList(classDataList)
+	return classDataList
+
+
+	#def calc_prob_class_data():
+
+#Print a list
+def printList(list):
+
+	#Printing the list 
+	for i in range(len(list)):
+		print(list[i])
+
+	return 0
 
 
 
 print("Iniciando el programa...")
-data = generar_muestra_pais(5)
-x = DecisionTree()
-x.calc_entropy()
+data_sample_size = 5
+sample_data = generar_muestra_pais(data_sample_size)
+#x = DecisionTree()
+calc_class_data(1,data)
