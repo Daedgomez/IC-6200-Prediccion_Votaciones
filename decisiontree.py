dd40beb4d68360b1c449376d91202b2807ad3441
Brayan <brayan.f.a07@hotmail.com>
2018-04-27 23:35:39 -0600

    Cambios realizados para cálculo de entropía con exito

--- decisiontree.py
+++ decisiontree.py
@@ -1,6 +1,8 @@
 from tec.ic.ia.pc1.g09 import generar_muestra_pais, generar_muestra_provincia
 
-list_political_parties=["ACCESIBILIDAD SIN EXCLUSION", "ACCION CIUDADANA", "ALIANZA DEMOCRATA CRISTIANA", 
+import math
+
+political_parties_list=["ACCESIBILIDAD SIN EXCLUSION", "ACCION CIUDADANA", "ALIANZA DEMOCRATA CRISTIANA", 
 "DE LOS TRABAJADORES", "FRENTE AMPLIO	INTEGRACION NACIONAL", "LIBERACION NACIONAL", "MOVIMIENTO LIBERTARIO", 
 "NUEVA GENERACION", "RENOVACION COSTARRICENSE", "REPUBLICANO SOCIAL CRISTIANO", "RESTAURACION NACIONAL", 
 "UNIDAD SOCIAL CRISTIANA"]
@@ -26,56 +28,63 @@ list_political_parties=["ACCESIBILIDAD SIN EXCLUSION", "ACCION CIUDADANA", "ALIA
 ##Calc the entropy of the attributes
 def calc_entropy():
 	
-	attributes_list = calc_class_data(0, data)
-	probalistic_class_data = []
+	class_type = 1 #Type of the class of the attribute to be analized
+	class_list = calc_class_data(class_type, sample_data) #List of the classes
+	probabilistic_class_list = [] #List of the probabilities of the every class
+	entropy = 0.0
 
-	for i in range(len(attributes_list)):
-		probalistic_class_data.append(probability_in_list(attributes_list[i], attributes_list))
+	#Calculate the probability of the every class type to be in the sample data
+	for i in range(len(class_list)):
+		probabilistic_class_list.append(probability_in_list(class_list[i], sample_data, class_type))
+		print("Class " + class_list[i] + ": " + str(probabilistic_class_list[i]))
 
 
+	base = 2.0
+	for i in range(len(class_list)):
+		entropy = entropy - (probabilistic_class_list[i] * (math.log(base,probabilistic_class_list[i])))
 
-	return list_attributes
+	print("Entropy of the class: " + str(class_type) + "= " + str(entropy))
+
+
+	return entropy
 
 #Calculate the probablistic to be found in a list
-def probability_in_list(element, list):
+def probability_in_list(element, list, position):
 
 	data_count = len(list)
-	probabability = found_count_in_list(element, list) / data_count
+	probabability = found_count_in_list(element, list, position) / data_count
 
 	return probabability
 
 #Calculate the count of the elements in a list
-def found_count_in_list(element, list):
+def found_count_in_list(element, list, position):
 
 	count = 0
 	for i in range(len(list)):
-		if (element == list[i]):
+		if (element == list[i][position]):
 			count = count + 1
 	return count
 
 
 #Calc the class of an object
-def calc_class_data(position, dataList):
+def calc_class_data(position, data_list):
 	
 
 	#Insert kind of attributes inside the list_attributes
-	classDataList = []
-	class_data_found_coun = []
+	class_data_list = []
 
-	for i in range(len(dataList)):
+	for i in range(len(data_list)):
 		found = False
-		for j in range(len(classDataList)):
-			if (classDataList[j] == dataList[i][position]):
+		for j in range(len(class_data_list)):
+			if (class_data_list[j] == data_list[i][position]):
 				found = True
 
 		if found == False:
-			classDataList.append(dataList[i][position])
+			class_data_list.append(data_list[i][position])
 
-	printList(classDataList)
-	return classDataList
+	printList(class_data_list)
+	return class_data_list
 
-
-	#def calc_prob_class_data():
 
 #Print a list
 def printList(list):
@@ -89,7 +98,7 @@ def printList(list):
 
 
 print("Iniciando el programa...")
-data_sample_size = 5
-sample_data = generar_muestra_pais(data_sample_size)
+sample_data_size = 5
+sample_data = generar_muestra_pais(sample_data_size)
 #x = DecisionTree()
-calc_class_data(1,data)
+calc_entropy()
